e30b6384fc2a2973f6d0838e410cbcc929b5c78f
bhuvaneshwaran` m <bhuvaneshwaran.m@monkeyscience.in>
2026-04-28 01:57:30 +0530

    fix: setup a basic test vr environment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  devIndicators: false
+  devIndicators: false,
+  allowedDevOrigins: ["192.168.0.101"],
 };
 
 export default nextConfig;
